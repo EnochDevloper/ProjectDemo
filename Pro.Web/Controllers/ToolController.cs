f56c0633e53d04b011f29c349faf67f2349135fc
me <397487285@qq.com>
2019-12-06 09:34:28 +0800

    添加 未包含的文件

---
+++ Pro.Web/Controllers/ToolController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pro.Web.Controllers
+{
+    public class ToolController : Controller
+    {
+        // GET: Tool
+        public ActionResult Elevator()
+        {
+            return View();
+        }
+    }
+}
