77afc7604d5f6c384dd30cf1766a3d5b68ccda92
me <397487285@qq.com>
2020-07-09 16:10:00 +0800

    bug提交  确实文件提交

---
+++ Pro.WebApi/Models/LogHelper.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+
+namespace Pro.WebApi.Models
+{
+    /// <summary>
+    /// 写入日志文件
+    /// </summary>
+    public class LogHelper
+    {
+        private LogHelper()
+        {
+            SetConfig();
+        }
+
+        public static readonly log4net.ILog loginfo = log4net.LogManager.GetLogger("loginfo");
+
+        public static readonly log4net.ILog logerror = log4net.LogManager.GetLogger("logerror");
+
+        public static void SetConfig()
+        {
+            log4net.Config.XmlConfigurator.Configure();
+        }
+
+        public static void SetConfig(FileInfo configFile)
+        {
+            log4net.Config.XmlConfigurator.Configure(configFile);
+        }
+
+        public static void WriteLog(string info)
+        {
+            var isEnable = logerror.IsInfoEnabled;
+            //if (loginfo.IsInfoEnabled)
+            //{
+            loginfo.Info(info);
+            //}
+        }
+
+        public static void WriteLog(string info, Exception se)
+        {
+            var isEnable = logerror.IsErrorEnabled;
+            //if (logerror.IsErrorEnabled)
+            //{
+            logerror.Error(info, se);
+            //}
+        }
+    }
+}
